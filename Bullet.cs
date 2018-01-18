2e66c58bc8f88f0a50f6db11c412dc22c6c6fc38
W0mbatman <32222219+W0mbatman@users.noreply.github.com>
2018-01-17 19:44:23 -0500

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        if (this.tag == "Bullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject BulletPrefab;
+    public Transform BulletSpawn;
+
+    // Use this for initialization
+    void Start () {
+        gameObject.transform.parent = gameObject.GetComponentInParent<Transform>();
+        gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+        gameObject.GetComponent<Collider2D>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject Bullet = Instantiate(BulletPrefab, BulletSpawn.position, BulletSpawn.rotation);
+
+            Bullet.tag = "Bullet";
+            Bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(12f, 0);
+        }
+	}
+}
